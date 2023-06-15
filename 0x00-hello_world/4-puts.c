9c869bae0dc981e41764fa28e3356e2d134bff3b
StephanSerwaa <aduseistephanie50@gmail.com>
2023-06-15 13:04:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include < stdio.h
+/**
+ *  main . program that prints a line with put function
+ *Return:0
+ */
+int main(void)
+{
+puts("\Pirogramming is like building a multilingual puzzle");
+return(0);
+}
